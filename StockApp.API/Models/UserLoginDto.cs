1385d6284b48836b7cea947af746388dc2f26bb7
GabrielFePL <gabriel.larocca@fatec.sp.gov.br>
2024-06-22 18:25:14 -0300

    Adição do UserLoginDto e Alteração no AppSettings.json

---
+++ StockApp.API/Models/UserLoginDto.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace StockApp.API.Models
+{
+    public class UserLoginDto
+    {
+        [Required(ErrorMessage = "Email is Required")]
+        [EmailAddress(ErrorMessage = "Invalid Format Email Address")]
+        public string? Email { get; set; }
+
+        [Required(ErrorMessage = "Password is Required")]
+        [StringLength(20, ErrorMessage = "Password must be between 10 and 20 characters", MinimumLength = 10)]
+        public string Password { get; set; }
+    }
+}
