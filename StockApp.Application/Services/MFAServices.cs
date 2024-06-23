af810be4fccba9f73badf362dca24e1af0a27a12
LMVieira2 <lucas.vieira64@fatec.sp.gov.br>
2024-06-23 17:07:00 -0300

    MFA implementado

---
+++ StockApp.Application/Services/MFAServices.cs
@@ -0,0 +1,47 @@
+﻿using AutoMapper.Configuration.Annotations;
+using StockApp.Application.Interfaces;
+using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StockApp.Application.Services
+{
+    internal class MFAServices : IMFAService
+    {
+        private readonly ConcurrentDictionary<string, string> _mfaCodes = new ConcurrentDictionary<string, string>();
+
+        public string GenerateMFACode()
+        {
+            var code = new Random().Next(100000, 999999).ToString();
+            _mfaCodes.TryAdd(code, code);
+            return code;
+        }
+
+        public bool ValidateMFACode(string code, out string storedcode)
+        {
+            storedcode = string.Empty;
+            foreach (var item in _mfaCodes)
+            {
+                if (item.Value == code)
+                {
+                    storedcode = item.Key;
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        public void StoreCode(string key, string code)
+        {
+            _mfaCodes[key] = code;
+        }
+
+        public void RemoveCode(string key)
+        {
+            _mfaCodes.TryRemove(key, out _);
+        }
+    }
+}
