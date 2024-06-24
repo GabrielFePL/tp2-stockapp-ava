47efa624f1f653717509a06864085ee16bcb11cd
GabrielFePL <gabriel.larocca@fatec.sp.gov.br>
2024-06-23 23:51:26 -0300

    Criação do UserController.

---
+++ StockApp.API/Controllers/UserController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using StockApp.Domain.Interfaces;
+using StockApp.Domain.Entities;
+using StockApp.Application.DTOs;
+
+namespace StockApp.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class UserController : ControllerBase
+    {
+        private readonly IUserRepository _userRepository;
+
+        public UserController(IUserRepository userRepository)
+        {
+            _userRepository = userRepository;
+        }
+
+        [HttpPost("register")]
+        public async Task<IActionResult> Register([FromBody] UserRegisterDto userRegisterDto)
+        {
+            var user = new User
+            {
+                Username = userRegisterDto.Username,
+                Password = BCrypt.Net.BCrypt.HashPassword(userRegisterDto.Password),
+                Role = userRegisterDto.Role
+            };
+
+            await _userRepository.AddAsync(user);
+
+            return Ok(user);
+        }
+    }
+}
